24033c859c570c195f6e4f0629761e251ca69a84
singhjeevan30 <jeevans30121998@gmail.com>
2021-03-30 17:08:24 +0530

    Yoyo

--- docker-deploy/docker_push.sh
+++ docker-deploy/docker_push.sh
@@ -11,16 +11,14 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 	sudo ./aws/install
         export PATH=$PATH:$HOME/.local/bin
         eval $(aws ecr get-login --no-include-email --region ap-south-1)
-	docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) 037368072271.dkr.ecr.ap-south-1.amazonaws.com
+	#docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) 037368072271.dkr.ecr.ap-south-1.amazonaws.com
 	aws --version
         docker build -t "$LIVE_APP_NAME" .
-        #docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:$TRAVIS_COMMIT"
-        #docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:latest"
-        #docker push "$AWS_URL/$LIVE_APP_NAME:$TRAVIS_COMMIT"
-        #docker push "$AWS_URL/$LIVE_APP_NAME:latest"
+        docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:$TRAVIS_COMMIT"
+        docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:latest"
+        docker push "$AWS_URL/$LIVE_APP_NAME:$TRAVIS_COMMIT"
+        docker push "$AWS_URL/$LIVE_APP_NAME:latest"
 	#docker tag john 037368072271.dkr.ecr.ap-south-1.amazonaws.com/john
 	#docker push 037368072271.dkr.ecr.ap-south-1.amazonaws.com/john
-	docker tag john:latest public.ecr.aws/u2z3j2j0/john:latest
-	docker push public.ecr.aws/u2z3j2j0/john:latest
     fi
 fi
