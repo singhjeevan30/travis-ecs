b8869a1a5fa5d883767fe0ad3e34f4e9941b6a18
singhjeevan30 <jeevans30121998@gmail.com>
2021-03-30 18:00:04 +0530

    yoqooo

--- docker-deploy/docker_push.sh
+++ docker-deploy/docker_push.sh
@@ -5,13 +5,13 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ] ; then
         # Build and push
         docker --version
-        pip install --user awscli
-	#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-	#unzip awscliv2.zip
-	#sudo ./aws/install
+        #pip install --user awscli
+	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+	unzip awscliv2.zip
+	sudo ./aws/install
         export PATH=$PATH:$HOME/.local/bin
         eval $(aws ecr get-login --no-include-email --region ap-south-1)
-	docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) 037368072271.dkr.ecr.ap-south-1.amazonaws.com
+	#docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) 037368072271.dkr.ecr.ap-south-1.amazonaws.com
 	aws --version
         docker build -t "$LIVE_APP_NAME" .
         docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:$TRAVIS_COMMIT"
