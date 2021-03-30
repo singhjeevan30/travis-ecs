f9ba1e823da52019748abc7f027921dc48789598
singhjeevan30 <jeevans30121998@gmail.com>
2021-03-30 17:10:59 +0530

    fefe

--- docker-deploy/docker_push.sh
+++ docker-deploy/docker_push.sh
@@ -5,10 +5,10 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ] ; then
         # Build and push
         docker --version
-        #pip install --user awscli
-	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-	unzip awscliv2.zip
-	sudo ./aws/install
+        pip install --user awscli
+	#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+	#unzip awscliv2.zip
+	#sudo ./aws/install
         export PATH=$PATH:$HOME/.local/bin
         eval $(aws ecr get-login --no-include-email --region ap-south-1)
 	#docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) 037368072271.dkr.ecr.ap-south-1.amazonaws.com
