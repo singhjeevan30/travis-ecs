da89d8b59bc51f39f48946bc53f0f74f356abe00
singhjeevan30 <jeevans30121998@gmail.com>
2021-03-30 18:38:24 +0530

    yo

--- docker-deploy/docker_push.sh
+++ docker-deploy/docker_push.sh
@@ -5,14 +5,14 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ] ; then
         # Build and push
         docker --version
-        #pip install --user awscli
-	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-	unzip awscliv2.zip
-	sudo ./aws/install
+        pip install --user awscli
+	#curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+	#unzip awscliv2.zip
+	#sudo ./aws/install
         export PATH=$PATH:$HOME/.local/bin
-        eval $(/usr/local/bin/aws ecr get-login --no-include-email --region ap-south-1)
+        eval $(aws ecr get-login --no-include-email --region ap-south-1)
 	#docker login -u AWS -p $(/usr/local/bin/aws ecr get-login-password --region ap-south-1) 037368072271.dkr.ecr.ap-south-1.amazonaws.com
-	/usr/local/bin/aws --version
+	aws --version
         docker build -t "$LIVE_APP_NAME" .
         docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:$TRAVIS_COMMIT"
         docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:latest"
