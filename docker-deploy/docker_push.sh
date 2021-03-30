e404c9036b6ed871ff9ba139ac5314ea5e8d4361
singhjeevan30 <jeevans30121998@gmail.com>
2021-03-30 15:12:03 +0530

    hi ayuuuuuuush

--- docker-deploy/docker_push.sh
+++ docker-deploy/docker_push.sh
@@ -9,9 +9,11 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
         export PATH=$PATH:$HOME/.local/bin
         eval $(aws ecr get-login --no-include-email --region ap-south-1)
         docker build -t "$LIVE_APP_NAME:latest" .
-        docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:$TRAVIS_COMMIT"
-        docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:latest"
-        docker push "$AWS_URL/$LIVE_APP_NAME:$TRAVIS_COMMIT"
-        docker push "$AWS_URL/$LIVE_APP_NAME:latest"
+        #docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:$TRAVIS_COMMIT"
+        #docker tag "$LIVE_APP_NAME:latest" "$AWS_URL/$LIVE_APP_NAME:latest"
+        #docker push "$AWS_URL/$LIVE_APP_NAME:$TRAVIS_COMMIT"
+        #docker push "$AWS_URL/$LIVE_APP_NAME:latest"
+	docker tag john 037368072271.dkr.ecr.ap-south-1.amazonaws.com/john
+	docker push 037368072271.dkr.ecr.ap-south-1.amazonaws.com/john
     fi
 fi
